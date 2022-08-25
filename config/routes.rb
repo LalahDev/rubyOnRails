c62698ab3253c87884583e9f0d1236cbe84f7761
Silvia <s.ff.9724@gmail.com>
2022-08-25 18:40:58 +0200

    Saving new articles without getting the params

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root "articles#index"
 
   get "/", to: "articles#index"
+  post "/", to: "articles#create"
 end
